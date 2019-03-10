b07a805b976b4d9b46897e18320c89840c8e102e
sattapong <philosophy2534@gmail.com>
2019-03-10 23:07:19 +0700

    first commit

---
+++ routes/register.js
@@ -0,0 +1,22 @@
+const turbo = require('turbo360')({site_id: process.env.TURBO_APP_ID})
+const vertex = require('vertex360')({site_id: process.env.TURBO_APP_ID})
+const router = vertex.router()
+
+
+
+router.post('/user',(req,res) => {
+const data = req.body
+
+
+res.json({
+    confirmation: 'success',
+    data:data
+})
+
+})
+
+
+
+
+
+module.exports = router
